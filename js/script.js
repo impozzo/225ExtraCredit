81e1a9656f0199b78138aff7c8d4d079f065c8d9
Michael Weisinger <impozzo@verizon.net>
2020-12-17 10:53:48 -0500

    Complete

--- js/script.js
+++ js/script.js
@@ -3,35 +3,41 @@ jQuery(document).ready(function ($) {
 
     jQuery('#get-weather').on('click', function () {
 
-        const zipcode = '10312';
+        const zipcode = $("input#zipcode").val();
         const apiKey = 'd636d1643ef0d31c83cfa378324cd157';
         const country = 'us';
         const weatherApiUrl = `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode},${country}&appid=${apiKey}`;
 
+        //show spinner
+        $('#spin').toggleClass('d-none');
+
 
         axios.get(weatherApiUrl).then(function (response) {
-
+            //Hide spinner
+            $('#spin').toggleClass('d-none');
             const {weather, main, name} = response.data;
             const {temp, feels_like} = main;
             const { description, icon} = weather[0];
             tempFarenheight = Math.floor((temp - 273.15) * (9/5) + 32);
-            console.log(tempFarenheight);
+            console.log(zipcode);
             console.log(response.data);
 
-            const weatherHtml = `<div class="container bg-info">
-            <div class="row">
-            <h4>${name}</h4>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <img src="http://openweathermap.org/img/wn/${icon}@2x.png">
+            const weatherHtml = `
+            <div class="container  bg-info rounded">
+                <div class="row">
+                    <h4>${name}</h4>
                 </div>
-                <div class="col">
-                    <p>Today: ${description}</p>
-                     <p>Temperature: ${tempFarenheight}&#176;</p>
+                <div class="row">
+                    <div class="col">
+                        <img src="http://openweathermap.org/img/wn/${icon}@2x.png">
+                    </div>
+                    <div class="col">
+                        <p>Today: ${description}</p>
+                        <p>Temperature: ${tempFarenheight}&#176;</p>
+                    </div>
                 </div>
             </div>
-        </div>`;
+        `;
         $('#response-weather').html(weatherHtml);
 
         }).catch(function (error) {
@@ -39,41 +45,6 @@ jQuery(document).ready(function ($) {
         });
 
 
-        /*  const id = $(this).attr('data-id');
-         
-         const loaderImage = `<img src="/images/ajax-page-loader.gif" alt="Loading Page"><p>Retreiving Information</p>`;
- 
-         // Empty #console div
-         $('#console').html('');
- 
-         // Show loader image and text
-         $('#console').html(loaderImage);
- 
- 
-         axios.get(consoleUrl).then(function (response) {
- 
-             //Clear loader image and text
-             $('#console').html('');
- 
-             const { id, name, price, country, releaseYear, image } = response.data;
- 
-             // Insert the Information details card into #console div
-             $('#console').html(`
-             <div class="container">
-                 <div class="card border-dark" style="width: 18rem;">
-                     <img src="${image}" alt="Photo of ${name}">
-                     <div class="card-body">
-                         <p class="m-0">Name: ${name}</p>
-                         <p class="m-0">Price: $${price}</p>
-                         <p class="m-0">Country: ${country}</p>
-                         <p class="m-0">Release Year: ${releaseYear}</p>
-                     </div>
-                 </div>
-             </div>`); */
-        /* 
-                }).catch(function (error) {
-                    // Something better should happen here
-                    alert(error);
-                }); */
+       
     });
 });
